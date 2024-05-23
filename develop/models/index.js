d641c17a6d96713663595aaa7be29d2d167e7a00
Sam <sambolton.kw@gmail.com>
2024-05-22 20:29:54 -0400

    index.js relationships model

--- develop/models/index.js
+++ develop/models/index.js
@@ -2,4 +2,34 @@ const User = require('./user');
 const Comment = require('./comment');
 const BlogPost = require('./post');
 
+//creating associations
+User.hasMany(BlogPost, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE',
+});
+
+BlogPost.hasMany(Comment, {
+    foreignKey: 'blogPostId',
+    onDelete: 'CASCADE',
+});
+
+BlogPost.belongsTo(User, {
+    foreignKey: 'userId'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'userId'
+});
+
+Comment.belongsTo(BlogPost, {
+    foreignKey: 'blogPostId'
+});
+
+
 module.exports = { User, Comment, BlogPost };
+
