372d709d7e109519005ec3c2af30b435ed027f16
Sam <sambolton.kw@gmail.com>
2024-05-24 18:15:45 -0400

    re-organize file structure

--- develop/controllers/index.js
+++ develop/controllers/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
-const apiRoutes = require('./api/');
-const dashboardRoutes = require('./dashboardRoutes');
+//const apiRoutes = require('./api/');
+//const dashboardRoutes = require('./dashboardRoutes');
 //establishing routing paths
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
+//router.use('/api', apiRoutes);
+//router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
---
+++ develop/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Handlebars.js w/ helper
+const hbs = exphbs.create({ helpers });
+
+//Set session and conntect to Sequelize
+
+const sess = {
+    secret: 'topSecret',
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+// activate handlebars engine and set it as default
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('./public'));
+
+app.use(routes);
+
+//sync sequalize models and start server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on port ${PORT}'))
+});
+
