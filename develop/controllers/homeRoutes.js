4cecaebde3a11b7d8ad7c62ea671f2ccf00ebbb5
Sam <sambolton.kw@gmail.com>
2024-05-24 17:24:56 -0400

    get route for home

--- develop/controllers/homeRoutes.js
+++ develop/controllers/homeRoutes.js
@@ -3,5 +3,23 @@ const router = require('express').Router();
 const { Comment, BlogPost, User } = require('../models');
 const withAuth = require('../utils/auth');
 //need to write this file
+//Get all post and render to home.handlbars
+router.get('/', async (req, res) => {
+    try{
+        const blogPostData = await BlogPost.findAll({
+            include: [User],
+        });
+        //serialze data
+        const posts = blogPostData.map((post) =>
+        post.get({ plain: true}));
 
+        //pass serialized data and session to home
+        res.render('home', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err){
+        res.status(500).json(err);
+    }
+})
 module.exports = router
