ff3ce4483461ffa576f10435f8d3b8c4c450db2f
Sam <sambolton.kw@gmail.com>
2024-05-24 15:56:41 -0400

    seeds I beleive is done

--- develop/seeds/seeds.js
+++ develop/seeds/seeds.js
@@ -5,5 +5,18 @@ const userData = require('./userData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true});
-    
-}
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const post of postData) {
+        await BlogPost.create({
+            ...post,
+            userId: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+    process.exit(0);
+};
+seedDatabase();
