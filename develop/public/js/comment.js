62e2306717b22a8da0f3675b5951164afb9668c3
Sam <sambolton.kw@gmail.com>
2024-05-25 15:02:26 -0400

    public comment.js should be done, uncommented api routes in controller index, comment routes should be good

--- develop/public/js/comment.js
+++ develop/public/js/comment.js
@@ -1,5 +1,30 @@
 const newCommentHandler = async (event) => {
     event.preventDefault();
 
-    
-}
+    const postId = document.querySelector('#post-id').value.trim();
+    const content = document.querySelector('#comment-text').value.trim();
+
+    if (content) {
+        const response = await fetch('/api/comment', {
+            method: 'POST',
+            body: JSON.stringify({ 
+                postId,
+                content,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            document.location.replace('/login')
+        }
+    }
+
+};
+
+document
+    .querySelector('#new-comment-form')
+    .addEventListener('submit', newCommentHandler);
