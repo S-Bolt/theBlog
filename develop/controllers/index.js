3da77d09fcbcba75622270d57cd13f9fde7df870
Sam <sambolton.kw@gmail.com>
2024-05-25 23:52:25 -0400

    dashboard.handlbars and opened up controller route for dashboard

--- develop/controllers/index.js
+++ develop/controllers/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+
 const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api/');
-//const dashboardRoutes = require('./dashboardRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 //establishing routing paths
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-//router.use('/dashboard', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
