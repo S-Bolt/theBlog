2f0dbc07f29251bb981e33bfda0f81cd2de145e4
Sam <sambolton.kw@gmail.com>
2024-05-26 21:46:05 -0400

    created newPost.handlebars html and created /dashboard/new route in dashboard.js controller

--- develop/controllers/dashboardRoutes.js
+++ develop/controllers/dashboardRoutes.js
@@ -30,4 +30,11 @@ router.get('/', async (req, res) => {
         }
 });
 
+router.get('/new', (req, res) => {
+    res.render('newPost', {
+        dashboard: true,
+        logged_in: req.session.logged_in,
+    });
+});
+
 module.exports = router;
